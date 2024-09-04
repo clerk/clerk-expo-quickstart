8022e9bb0da06801b335dfdc54108808d26d849b
Kenton Duprey <kenton@clerk.dev>
2024-09-04 10:23:21 -0400

    feat: adds warmup browser for OAuth, err handling on sign in/up, back btn, layout styling
    Adds error handling with messages displayed to user if input is incorrect.
    Adds styling to inputs with color on placeholder text and borders on inputs. Configures
    autocomplete for inputs.
    Updates Links to Buttons for standard styling.
    
    Signed-off-by: Kenton Duprey <kenton@clerk.dev>

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,60 +1,164 @@
-import { useSignIn } from "@clerk/clerk-expo";
-import { Link, useRouter } from "expo-router";
-import { Text, TextInput, Button, View } from "react-native";
-import React from "react";
+import { useSignIn } from '@clerk/clerk-expo'
+import { Stack, useRouter } from 'expo-router'
+import { Text, TextInput, Button, View } from 'react-native'
+import React from 'react'
+import * as WebBrowser from 'expo-web-browser'
+
+export const useWarmUpBrowser = () => {
+  React.useEffect(() => {
+    // Warm up the android browser to improve UX
+    // https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync()
+    return () => {
+      void WebBrowser.coolDownAsync()
+    }
+  }, [])
+}
+
+WebBrowser.maybeCompleteAuthSession()
 
 export default function Page() {
-  const { signIn, setActive, isLoaded } = useSignIn();
-  const router = useRouter();
+  const { signIn, setActive, isLoaded } = useSignIn()
+  const router = useRouter()
 
-  const [emailAddress, setEmailAddress] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [emailAddress, setEmailAddress] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [emailError, setEmailError] = React.useState('')
+  const [passwordError, setPasswordError] = React.useState('')
 
   const onSignInPress = React.useCallback(async () => {
     if (!isLoaded) {
-      return;
+      return
+    }
+
+    if (!emailAddress) {
+      setEmailError('Email is required')
+    }
+    if (!password) {
+      setPasswordError('Password is required')
+    }
+
+    if (!emailAddress || !password) {
+      return
     }
 
     try {
       const signInAttempt = await signIn.create({
         identifier: emailAddress,
         password,
-      });
+      })
 
-      if (signInAttempt.status === "complete") {
-        await setActive({ session: signInAttempt.createdSessionId });
-        router.replace("/");
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId })
       } else {
         // See https://clerk.com/docs/custom-flows/error-handling
         // for more info on error handling
-        console.error(JSON.stringify(signInAttempt, null, 2));
+        console.error(JSON.stringify(signInAttempt, null, 2))
       }
     } catch (err: any) {
-      console.error(JSON.stringify(err, null, 2));
+      console.error(JSON.stringify(err, null, 2))
     }
-  }, [isLoaded, emailAddress, password]);
+  }, [isLoaded, emailAddress, password])
 
   return (
     <View>
+      <Stack.Screen
+        options={{
+          title: '',
+          headerLeft: () => <Button title="Back" onPress={() => router.replace('/')} />,
+        }}
+      />
+
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          marginVertical: 10,
+        }}
+      >
+        Sign In
+      </Text>
+      <View>
+        {emailError ? (
+          <Text
+            style={{
+              color: 'red',
+              marginBottom: 10,
+              fontWeight: 'bold',
+              fontSize: 16,
+              textAlign: 'center',
+            }}
+          >
+            {emailError}
+          </Text>
+        ) : null}
+        {passwordError ? (
+          <Text
+            style={{
+              color: 'red',
+              marginBottom: 10,
+              fontWeight: 'bold',
+              fontSize: 16,
+              textAlign: 'center',
+            }}
+          >
+            {passwordError}
+          </Text>
+        ) : null}
+      </View>
       <TextInput
         autoCapitalize="none"
+        placeholderTextColor={'#000'}
         value={emailAddress}
         placeholder="Email..."
+        autoComplete="email"
+        inputMode="email"
+        style={{
+          marginBottom: 10,
+          marginHorizontal: 10,
+          borderColor: 'black',
+          borderWidth: 1,
+          padding: 10,
+        }}
         onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
       />
       <TextInput
         value={password}
         placeholder="Password..."
+        placeholderTextColor={'#000'}
+        autoComplete="password"
+        style={{
+          marginBottom: 10,
+          marginHorizontal: 10,
+          borderColor: 'black',
+          borderWidth: 1,
+          padding: 10,
+        }}
         secureTextEntry={true}
         onChangeText={(password) => setPassword(password)}
       />
       <Button title="Sign In" onPress={onSignInPress} />
-      <View>
-        <Text>Don't have an account?</Text>
-        <Link href="/sign-up">
-          <Text>Sign up</Text>
-        </Link>
+
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          paddingTop: 10,
+        }}
+      >
+        <Text
+          style={{
+            marginTop: 10,
+            marginBottom: 10,
+            fontSize: 20,
+          }}
+        >
+          Don't have an account?
+        </Text>
+        <Button onPress={() => router.replace('/sign-up')} title="Sign Up" />
       </View>
     </View>
-  );
+  )
 }
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,66 +1,141 @@
-import * as React from "react";
-import { TextInput, Button, View } from "react-native";
-import { useSignUp } from "@clerk/clerk-expo";
-import { useRouter } from "expo-router";
+import * as React from 'react'
+import { TextInput, Button, View, Text } from 'react-native'
+import { useSignUp } from '@clerk/clerk-expo'
+import { Stack, useRouter } from 'expo-router'
 
 export default function SignUpScreen() {
-  const { isLoaded, signUp, setActive } = useSignUp();
-  const router = useRouter();
+  const { isLoaded, signUp, setActive } = useSignUp()
+  const router = useRouter()
 
-  const [emailAddress, setEmailAddress] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [pendingVerification, setPendingVerification] = React.useState(false);
-  const [code, setCode] = React.useState("");
+  const [emailAddress, setEmailAddress] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [pendingVerification, setPendingVerification] = React.useState(false)
+  const [code, setCode] = React.useState('')
+  const [emailError, setEmailError] = React.useState('')
+  const [passwordError, setPasswordError] = React.useState('')
+  const [otpError, setOtpError] = React.useState('')
 
   const onSignUpPress = async () => {
     if (!isLoaded) {
-      return;
+      return
+    }
+
+    if (!emailAddress) {
+      setEmailError('Email is required')
+    }
+    if (!password) {
+      setPasswordError('Password is required')
+    }
+
+    if (!emailAddress || !password) {
+      return
     }
 
     try {
       await signUp.create({
         emailAddress,
         password,
-      });
+      })
 
-      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+      await signUp.prepareEmailAddressVerification({
+        strategy: 'email_code',
+      })
 
-      setPendingVerification(true);
+      setPendingVerification(true)
     } catch (err: any) {
       // See https://clerk.com/docs/custom-flows/error-handling
       // for more info on error handling
-      console.error(JSON.stringify(err, null, 2));
+      console.error(JSON.stringify(err, null, 2))
     }
-  };
+  }
 
   const onPressVerify = async () => {
     if (!isLoaded) {
-      return;
+      return
+    }
+
+    if (!code) {
+      setOtpError('OTP is required')
+      return
     }
 
     try {
       const completeSignUp = await signUp.attemptEmailAddressVerification({
         code,
-      });
+      })
 
-      if (completeSignUp.status === "complete") {
-        await setActive({ session: completeSignUp.createdSessionId });
-        router.replace("/");
+      if (completeSignUp.status === 'complete') {
+        await setActive({ session: completeSignUp.createdSessionId })
+        router.replace('/')
       } else {
-        console.error(JSON.stringify(completeSignUp, null, 2));
+        console.error(JSON.stringify(completeSignUp, null, 2))
       }
     } catch (err: any) {
       // See https://clerk.com/docs/custom-flows/error-handling
       // for more info on error handling
-      console.error(JSON.stringify(err, null, 2));
+      console.error(JSON.stringify(err, null, 2))
     }
-  };
+  }
 
   return (
     <View>
+      <Stack.Screen
+        options={{
+          title: '',
+          headerLeft: () => <Button title="Back" onPress={() => router.replace('/')} />,
+        }}
+      />
       {!pendingVerification && (
         <>
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: 'bold',
+              textAlign: 'center',
+              marginVertical: 10,
+            }}
+          >
+            Sign Up
+          </Text>
+          <View>
+            {emailError ? (
+              <Text
+                style={{
+                  color: 'red',
+                  marginBottom: 10,
+                  fontWeight: 'bold',
+                  fontSize: 16,
+                  textAlign: 'center',
+                }}
+              >
+                {emailError}
+              </Text>
+            ) : null}
+            {passwordError ? (
+              <Text
+                style={{
+                  color: 'red',
+                  marginBottom: 10,
+                  fontWeight: 'bold',
+                  fontSize: 16,
+                  textAlign: 'center',
+                }}
+              >
+                {passwordError}
+              </Text>
+            ) : null}
+          </View>
           <TextInput
+            style={{
+              marginBottom: 10,
+              marginHorizontal: 10,
+              borderColor: 'black',
+              borderWidth: 1,
+              padding: 10,
+            }}
+            inputMode="email"
+            placeholderTextColor={'#000'}
+            autoComplete="email"
             autoCapitalize="none"
             value={emailAddress}
             placeholder="Email..."
@@ -68,7 +143,16 @@ export default function SignUpScreen() {
           />
           <TextInput
             value={password}
+            style={{
+              marginBottom: 10,
+              marginHorizontal: 10,
+              borderColor: 'black',
+              borderWidth: 1,
+              padding: 10,
+            }}
             placeholder="Password..."
+            autoComplete="password-new"
+            placeholderTextColor={'#000'}
             secureTextEntry={true}
             onChangeText={(password) => setPassword(password)}
           />
@@ -77,7 +161,38 @@ export default function SignUpScreen() {
       )}
       {pendingVerification && (
         <>
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: 'bold',
+              textAlign: 'center',
+              marginVertical: 10,
+            }}
+          >
+            We have sent you an email with a verification code. Please enter the code below.
+          </Text>
+          {otpError ? (
+            <Text
+              style={{
+                color: 'red',
+                marginBottom: 10,
+                fontWeight: 'bold',
+                fontSize: 16,
+                textAlign: 'center',
+              }}
+            >
+              {otpError}
+            </Text>
+          ) : null}
           <TextInput
+            style={{
+              marginBottom: 10,
+              marginHorizontal: 10,
+              borderColor: 'black',
+              borderWidth: 1,
+              padding: 10,
+            }}
+            autoComplete="one-time-code"
             value={code}
             placeholder="Code..."
             onChangeText={(code) => setCode(code)}
@@ -86,5 +201,5 @@ export default function SignUpScreen() {
         </>
       )}
     </View>
-  );
+  )
 }
